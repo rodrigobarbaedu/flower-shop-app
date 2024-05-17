396384897ebc6b4f8123a61ec55ac2a737c645c4
Luis Rodrigo Barba Navarro <78876540+LuisRodrigoBarbaNavarro@users.noreply.github.com>
2024-05-16 20:00:30 -0700

    Update: Some Features

--- source/static/js/script-receipts.js
+++ source/static/js/script-receipts.js
@@ -166,22 +166,7 @@ $(document).ready(function() {
     $('#product-table').DataTable({
       "paging": true,        // Habilita la paginación
       "searching": true,     // Habilita la búsqueda
-      "pageLength": 3,       // Se muestran 3 filas por página
+      "pageLength": 8,       // Se muestran 3 filas por página
       "lengthMenu": [3, 6, 9, 12], // Se puede elegir mostrar 3, 6, 9 o 12 filas por página
-      "language": {          // Se configura el idioma del plugin
-          "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
-      },
-      "scrollX": "100%",
-      // Definir tamaños equivalentes para las columnas
-      "columnDefs": [
-        { "width": "15%", "targets": 0 },
-        { "width": "15%", "targets": 1 },
-        { "width": "20%", "targets": 2 },
-        { "width": "10%", "targets": 3 },
-        { "width": "20%", "targets": 4 }
-      ],
-      "ordering": true,      // Habilita la ordenación
-      "info": true,          // Muestra información sobre la paginación
-      "lengthChange": true   // Permite cambiar el número de elementos mostrados por página
     });
 });
